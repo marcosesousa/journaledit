391cad05799e3d58282c4d0f052e0bac1cb53ae9
marcos <25065425+marcosesousa@users.noreply.github.com>
2022-11-25 10:44:17 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'journaledit'
+copyright = '2022, marcosesousa'
+author = 'marcos e sousa'
 
 release = '0.1'
 version = '0.1.0'
